679f14d02893716bb3cdbfc15cbd18c3d51e2efd
Scott Jackson <scottjacksonx@gmail.com>
2011-04-20 17:31:22 +1000

    Twitter completed.

---
+++ twitter.py
@@ -0,0 +1,23 @@
+try:
+	import json
+except:
+	import simplejson as json
+import urllib2
+
+def getFavorites(user, favs=200):
+	"""
+	"""
+	url = "http://twitter.com/favorites/" + user + ".json?count=" + str(favs)
+	try:
+		req = urllib2.urlopen(url)
+		print req
+		return json.loads(req.read())
+	except urllib2.HTTPError, e:
+		if e.code == 401:
+			return "nofaves"
+		if e.code == 404:
+			return "nofaves"
+		return "400_status_code_received"
+
+# Testing Code #
+print getFavorites("scottjacksonx")
